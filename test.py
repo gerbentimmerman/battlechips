cee4fb3f7b84439edbb95f111ea5bb0a3cf6634c
gerben540 <gerben_timmerman@hotmail.com>
2015-03-25 02:15:11 -0700

    test.py updated

--- test.py
+++ test.py
@@ -69,20 +69,17 @@ class SchepenKiezen(self):
 		return self.begincoord1
 		
 	def eindschip(self, lengte):
-		self.eindcoord1 = #input coord + info over de lengte
-		self.lengte1 = lengte
+		self.lengte = lengte
+		self.eindcoord1 = QtGui.QInputDialog.getText(self, 'Schiet Coordinaat', 'Vul hier het eindcoordinaat wat mag' + self.lengte + 'coordinaten van het begincoordinaat afligt in.')
 		return self.eindcoord1, self.lengte1
 	
 	def schepenGebruiker(self):
-		# er moet nog een combobox gemaakt worden genaamd self.horizonverticaalbox
-		# waarin de keuze tussen verticaal en horizontaal gemaakt moet worden.
-		
 		# lijst met daarin de lengtes van de schepen waar over je indenteert.
 		self.lijstlengtes = [2,3,3,4,5]
 		self.lijstSchepenGebruiker = []
 		for item in self.lijstlengtes:
 			self.begincoord = beginschip()
-			self.eindcoord1, lengte = eindschip1(item)
+			self.eindcoord, lengte = eindschip(item)
 			counter = 0
 			x,y = self.begincoord[0], self.begincoord[1]
 			self.tussencoord = (x,y)
@@ -91,6 +88,7 @@ class SchepenKiezen(self):
 					self.tussencoord = (x,y+1)
 					self.lijstSchepenGebruiker.append(self.tussencoord)
 					counter += 1
+				self.lijstSchepenGebruiker.append(self.eindcoord)
 			if self.horizonverticaalbox.currentText() == "Verticaal":
 				while self.tussencoord != self.eindcoord1:
 					self.tussencoord = (x+1,y)
@@ -99,13 +97,32 @@ class SchepenKiezen(self):
 			if counter != lengte:
 				self.lijstSchepenGebruiker = []
 				self.schepengebruiker()
+			# Aanpassen fout
 			if self.begincoord or self.tussencoord or self.eindcoord in self.lijstSchepenGebruiker:
 				self.lijstSchepenGebruiker = []
 				self.schepenGebruiker()
 		
 		
 	def schepenComputer(self):
-		self.startcoordcomputerschip = (randrange(1,11),
+		self.lijstlengtes = [2,3,3,4,5]
+		for item in self.lijstlengtes:
+			self.begincoordcomputerschip = (randrange(1,11),randrange(1,11))
+			self.lijstSchepenComputer.append(self.begincoordcomputerschip)
+			(x,y) = self.begincoordcomputerschip[0], self.begincoordcomputerschip[1]
+			hv = randrange(2)
+			if hv == 0:
+				self.eindcoordcomputerschip = (x,y + item)
+				while self.begincoordcomputerschip != self.eindcoordcomputerschip:
+					self.begincoordcomputerschip = (x,y+1)
+					self.lijstSchepenComputer.append(self.begincoordcomputerschip)
+				
+			if hv == 1:
+				self.eindcoordcomputerschip = (x + item,y)
+				while self.begincoordcomputerschip != self.eindcoordcomputerschip:
+					self.begincoordcomputerschip = (x+1,y)
+					self.lijstSchepenComputer.append(self.begincoordcomputerschip)
+			self.lijstSchepenComputer.append(self.eindcoordcomputerschip)
+			if self.begincoordcomputer
 		#volgt morgen
 		
 		
